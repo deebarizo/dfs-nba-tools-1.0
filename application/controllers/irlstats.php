8bf2b1a01479b12d8e6bb567c11e4aba3140b16e
deebarizo <deebarizo@gmail.com>
2013-12-20 17:55:25 -0600

    Generated espn game urls for scraping

--- application/controllers/irlstats.php
+++ application/controllers/irlstats.php
@@ -7,7 +7,55 @@ class Irlstats extends CI_Controller
 	{
 		$date_range = $this->create_date_range_array('2013-10-29', '2013-12-19');
 
-		echo '<pre>'; var_dump($date_range); echo '</pre>'; exit();
+		// echo '<pre>'; var_dump($date_range); echo '</pre>'; exit();
+
+		$this->load->helper('phpquery');
+
+		foreach ($date_range as $key => $date) 
+		{
+			$date_segment = preg_replace("/\-/", "", $date);
+
+			$html = phpQuery::newDocumentFileHTML('http://scores.espn.go.com/nba/scoreboard?date='.$date_segment);
+
+			$count = $html->find('.expand-gameLinks');
+			$num_urls = count($count);
+
+			if ($num_urls > 0)
+			{
+				$data_to_insert[$date_segment]['num_of_games'] = $num_urls;
+
+				for ($n=0; $n < $num_urls; $n++)
+				{
+					$url_segment = $html->find('div[class=expand-gameLinks]:eq('.$n.') a:first')->attr('href');
+					$data_to_insert[$date_segment]['games'][$n] = 'http://scores.espn.go.com'.$url_segment;
+				}
+			}
+			else
+			{
+				$data_to_insert[$date_segment]['num_of_games'] = $num_urls;
+			}
+		}
+
+		foreach ($data_to_insert as $key => $date) 
+		{
+			if ($date['num_of_games'] > 0)
+			{
+				echo $key.'<br>';
+
+				foreach ($date as $key2 => $games) 
+				{
+					if ($key2 === 'games')
+					{
+						foreach ($games as $game_url) 
+						{
+							echo $game_url.'<br>';
+						}
+					}
+				}
+			}
+		}
+
+		echo '<pre>'; var_dump($data_to_insert); echo '</pre>'; exit();
 	}
 
 	function create_date_range_array($strDateFrom,$strDateTo)
