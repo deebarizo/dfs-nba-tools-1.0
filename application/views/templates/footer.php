bc77724c3e195ae8da034bec6466db277b750e4a
deebarizo <deebarizo@gmail.com>
2013-12-31 19:45:08 -0600

    Refactoring options javascript

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -49,7 +49,16 @@
 			if (position == 'center') { return 'position-C'; }			
 		}
 
-		function get_games()
+		function get_teams_in_game(two_teams)
+		{
+			var team = new Object();
+			team[1] = two_teams.split(/ vs /)[0];
+			team[2] = two_teams.split(/ vs /)[1];
+
+			return team;
+		}
+
+		function get_teams()
 		{
 			var teams = new Array();
 
@@ -63,30 +72,23 @@
 				teams.push(team[2]);
 			});	
 
-			console.log(teams);
-		}
-
-		get_games();
-
-		function get_teams_in_game(two_teams)
-		{
-			var team = new Object();
-			team[1] = two_teams.split(/ vs /)[0];
-			team[2] = two_teams.split(/ vs /)[1];
-
-			console.log(team);
-
-			return team;
+			return teams;
 		}
 
 		function get_options()
 		{
 			var options = new Object();
 			options['position'] = get_position();
+			options['teams'] = get_teams();
 			return options;
 		}
 
-		$('select[name=position-drop-down]').change(function () 
+		options = get_options();
+
+		console.log(options['position']);
+		console.log(options['teams']);
+
+		$('select[name=position-drop-down]').change(function() 
 		{
 			
 		}); 
@@ -99,39 +101,39 @@
 
 			var options = get_options();
 
-			if ($(this).hasClass('hide-game'))
-			{
-				if (options['position'] == 'all')
+				if ($(this).hasClass('hide-game'))
 				{
-					$('.'+team[1]).addClass('show-row');
-					$('.'+team[1]).removeClass('hide-row');
+					if (options['position'] == 'all')
+					{
+						$('.'+team[1]).addClass('show-row');
+						$('.'+team[1]).removeClass('hide-row');
 
-					$('.'+team[2]).addClass('show-row');
-					$('.'+team[2]).removeClass('hide-row');		
-				}
-				else
+						$('.'+team[2]).addClass('show-row');
+						$('.'+team[2]).removeClass('hide-row');		
+					}
+					else
+					{
+						$('.'+team[1]+'.'+options['position']).addClass('show-row');
+						$('.'+team[1]+'.'+options['position']).removeClass('hide-row');
+
+						$('.'+team[2]+'.'+options['position']).addClass('show-row');
+						$('.'+team[2]+'.'+options['position']).removeClass('hide-row');
+					}
+
+					$(this).addClass('show-game');
+					$(this).removeClass('hide-game');
+				} 
+				else if ($(this).hasClass('show-game'))
 				{
-					$('.'+team[1]+'.'+options['position']).addClass('show-row');
-					$('.'+team[1]+'.'+options['position']).removeClass('hide-row');
+					$('.'+team[1]).addClass('hide-row');
+					$('.'+team[1]).removeClass('show-row');
 
-					$('.'+team[2]+'.'+options['position']).addClass('show-row');
-					$('.'+team[2]+'.'+options['position']).removeClass('hide-row');
+					$('.'+team[2]).addClass('hide-row');
+					$('.'+team[2]).removeClass('show-row');
+
+					$(this).addClass('hide-game');
+					$(this).removeClass('show-game');
 				}
-
-				$(this).addClass('show-game');
-				$(this).removeClass('hide-game');
-			} 
-			else if ($(this).hasClass('show-game'))
-			{
-				$('.'+team[1]).addClass('hide-row');
-				$('.'+team[1]).removeClass('show-row');
-
-				$('.'+team[2]).addClass('hide-row');
-				$('.'+team[2]).removeClass('show-row');
-
-				$(this).addClass('hide-game');
-				$(this).removeClass('show-game');
-			}
 
 			$('.show-row').show();
 			$('.hide-row').hide();
