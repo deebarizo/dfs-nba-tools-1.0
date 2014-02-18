7a611bac3332224e7acfe7a51a728b693c164eed
deebarizo <deebarizo@gmail.com>
2014-02-17 19:35:45 -0600

    Refactored teams model and added ratio fpt/pt of opponents

--- application/models/teams_model.php
+++ application/models/teams_model.php
@@ -79,28 +79,127 @@ class teams_model extends CI_Model
 
 		ini_set('max_execution_time', 10800); // 10800 seconds = 3 hours
 
-		$date_range = $this->create_date_range_array('2013-10-29', $latest_date_in_irlstats_db); 
+		$sql = 'SELECT `abbr_espn` as team
+				FROM `teams`';
+		$s = $this->db->conn_id->prepare($sql);
+		$s->execute(); 
 
-		foreach ($date_range as $key => $date) 
+		$teams = $s->fetchAll(PDO::FETCH_ASSOC);
+
+		$sql = 'SELECT `team2`, COUNT(`team2`) as num_games, SUM(`score2`) as total_pts, SUM(`score1`) AS total_pts_opp
+				FROM `games` 
+				WHERE `date` BETWEEN :starting_date AND :ending_date
+				GROUP BY `team2`
+				ORDER BY `team2`';
+		$s = $this->db->conn_id->prepare($sql);
+		$s->bindValue(':starting_date', '2013-10-29');
+		$s->bindValue(':ending_date', $latest_date_in_irlstats_db);
+		$s->execute(); 
+
+		$ppg['home'] = $s->fetchAll(PDO::FETCH_ASSOC);
+
+		$sql = 'SELECT `team1`, COUNT(`team1`) as num_games, SUM(`score1`) as total_pts, SUM(`score2`) AS total_pts_opp
+				FROM `games` 
+				WHERE `date` BETWEEN :starting_date AND :ending_date
+				GROUP BY `team1`
+				ORDER BY `team1`';
+		$s = $this->db->conn_id->prepare($sql);
+		$s->bindValue(':starting_date', '2013-10-29');
+		$s->bindValue(':ending_date', $latest_date_in_irlstats_db);
+		$s->execute(); 
+
+		$ppg['road'] = $s->fetchAll(PDO::FETCH_ASSOC);
+
+		$sql = 'SELECT `team`, SUM(`fpts_ds`) as total_fpts
+				FROM `irlstats` 
+				WHERE `date` BETWEEN :starting_date AND :ending_date
+				GROUP BY `team`
+				ORDER BY `team`';
+		$s = $this->db->conn_id->prepare($sql);
+		$s->bindValue(':starting_date', '2013-10-29');
+		$s->bindValue(':ending_date', $latest_date_in_irlstats_db);
+		$s->execute(); 
+
+		$fppg = $s->fetchAll(PDO::FETCH_ASSOC);		
+
+		$sql = 'SELECT `opponent`, SUM(`fpts_ds`) as total_fpts_opp
+				FROM `irlstats` 
+				WHERE `date` BETWEEN :starting_date AND :ending_date
+				GROUP BY `opponent`
+				ORDER BY `opponent`';
+		$s = $this->db->conn_id->prepare($sql);
+		$s->bindValue(':starting_date', '2013-10-29');
+		$s->bindValue(':ending_date', $latest_date_in_irlstats_db);
+		$s->execute(); 
+
+		$fppg_opp = $s->fetchAll(PDO::FETCH_ASSOC);		
+
+		foreach ($teams as &$row) 
 		{
-			$sql = 'SELECT `team1`, `score1`, `team2`, `score2`, `date` FROM `games` WHERE `date` = :date';
-			$s = $this->db->conn_id->prepare($sql);
-			$s->bindValue(':date', $date);
-			$s->execute(); 
-
-			$result = $s->fetchAll(PDO::FETCH_ASSOC);
-			if (empty($result) === false)
+			foreach ($ppg['home'] as $value) 
 			{
-				$schedule[$date] = $result;
+				if ($row['team'] == $value['team2'])
+				{
+					$row['home_num_games'] = $value['num_games'];
+					$row['home_total_pts'] = $value['total_pts'];
+					$row['home_total_pts_opp'] = $value['total_pts_opp'];
+
+					break;
+				}
 			}
+
+			foreach ($ppg['road'] as $value) 
+			{
+				if ($row['team'] == $value['team1'])
+				{
+					$row['road_num_games'] = $value['num_games'];
+					$row['road_total_pts'] = $value['total_pts'];
+					$row['road_total_pts_opp'] = $value['total_pts_opp'];
+
+					break;
+				}
+			}		
+
+			foreach ($fppg as $value) 
+			{
+				if ($row['team'] == $value['team'])
+				{
+					$row['total_fpts'] = $value['total_fpts'];
+
+					break;
+				}
+			}	
+
+			foreach ($fppg_opp as $value) 
+			{
+				if ($row['team'] == $value['opponent'])
+				{
+					$row['total_fpts_opp'] = $value['total_fpts_opp'];
+
+					break;
+				}
+			}		
 		}
 
+		unset($row);
+
+		foreach ($teams as &$row) 
+		{
+			$row['total_games'] = $row['home_num_games'] + $row['road_num_games'];
+			
+			$row['total_pts'] = $row['home_total_pts'] + $row['road_total_pts'];
+			$row['total_pts_opp'] = $row['home_total_pts_opp'] + $row['road_total_pts_opp'];
+
+			$row['ratio_fpt_per_pt'] = $row['total_fpts'] / $row['total_pts'];
+			$row['ratio_fpt_per_pt_opp'] = $row['total_fpts_opp'] / $row['total_pts_opp'];			
+		}
+
+		unset($row);
+
 		echo '<pre>'; 
-		# var_dump($team_stats);
+		var_dump($teams);
 		# var_dump($teams); 
 		# var_dump($correlation); 
-		# var_dump($stats);
-		var_dump($schedule);
 		echo '</pre>'; exit();
 
 		foreach ($schedule as &$games) 
