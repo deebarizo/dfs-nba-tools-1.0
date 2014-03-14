7d009d655dd47f241045b7ad7eeee845db59513a
deebarizo <deebarizo@gmail.com>
2014-03-13 20:18:54 -0500

    Finished update/fd, which scrapes FD salaries for the day

--- application/models/scraping_model.php
+++ application/models/scraping_model.php
@@ -39,17 +39,55 @@ class scraping_model extends CI_Model
 			{ 
 				$fstats_fd[$i]['name'] = $html->find('tr[data-role=player]:eq('.$i.')')->find('td:eq(1)')->text();
 				$fstats_fd[$i]['team'] = $html->find('tr[data-role=player]:eq('.$i.')')->find('td:eq(4)')->find('b')->text();
-				# $fstats_fd[$i]['position'] = $html->find('tr[data-role=player]:eq('.$i.')')->find('td:eq(1)')->text();
-				# $fstats_fd[$i]['salary'] = $html->find('tr[data-role=player]:eq('.$i.')')->find('td:eq(1)')->text();
-				# $fstats_fd[$i]['opponent'] = $html->find('tr[data-role=player]:eq('.$i.')')->find('td:eq(1)')->text();
-				# $fstats_fd[$i]['num_games'] = $html->find('tr[data-role=player]:eq('.$i.')')->find('td:eq(1)')->text();
-				# $fstats_fd[$i]['fppg'] = $html->find('tr[data-role=player]:eq('.$i.')')->find('td:eq(1)')->text();
-				# $fstats_fd[$i]['date'] = $html->find('tr[data-role=player]:eq('.$i.')')->find('td:eq(1)')->text();
+				$fstats_fd[$i]['position'] = $html->find('tr[data-role=player]:eq('.$i.')')->find('td:eq(0)')->text();
+				
+				$salary = $html->find('tr[data-role=player]:eq('.$i.')')->find('td:eq(5)')->text();
+				$salary = preg_replace("/\\$/", "", $salary);
+				$salary = preg_replace("/,/", "", $salary);
+				$fstats_fd[$i]['salary'] = $salary; 
+
+				$opponent = $html->find('tr[data-role=player]:eq('.$i.')')->find('td:eq(4)')->text();
+				$opponent = preg_replace("/@/", "", $opponent);
+				$opponent = preg_replace("/".$fstats_fd[$i]['team']."/", "", $opponent);
+				$fstats_fd[$i]['opponent'] = $opponent; 
+
+				$fstats_fd[$i]['num_games'] = $html->find('tr[data-role=player]:eq('.$i.')')->find('td:eq(3)')->text();
+				$fstats_fd[$i]['fppg'] = $html->find('tr[data-role=player]:eq('.$i.')')->find('td:eq(2)')->text();
 			}
 
-			echo '<pre>';
-			var_dump($fstats_fd);
-			echo '</pre>'; exit();				
+			# echo '<pre>';
+			# var_dump($fstats_fd);
+			# echo '</pre>'; exit();	
+
+			foreach ($fstats_fd as $key => $value) 
+			{
+				$sql = 'INSERT INTO `fstats_fd`(`name`, 
+												`team`, 
+												`position`,
+												`salary`,
+												`opponent`,
+												`num_games`,
+												`fppg`,
+												`date`) 
+						VALUES (:name, 
+								:team, 
+								:position,
+								:salary,
+								:opponent,
+								:num_games,
+								:fppg,
+								:date)'; 
+				$s = $this->db->conn_id->prepare($sql);
+				$s->bindValue(':name', $value['name']);
+				$s->bindValue(':team', $value['team']);
+				$s->bindValue(':position', $value['position']);
+				$s->bindValue(':salary', $value['salary']);
+				$s->bindValue(':opponent', $value['opponent']);
+				$s->bindValue(':num_games', $value['num_games']);
+				$s->bindValue(':fppg', $value['fppg']);
+				$s->bindValue(':date', $date);
+				$s->execute(); 
+			}				
 
 			return 'Success: The FD salaries for this date were scraped.';
 		}
